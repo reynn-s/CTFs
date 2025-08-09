1b395bda75c1bd24f3b7a9b5f5873171622c8360
Reyandanu Fajri Daniswara <reyangames@gmail.com>
2025-08-09 19:14:20 +0700

    Update Setup.sol

--- 2025/HackerClass/Blockchain/S3CR3T/src/Setup.sol
+++ 2025/HackerClass/Blockchain/S3CR3T/src/Setup.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "./Secret.sol";
+
+contract Setup {
+    Secret public challenge;
+
+    constructor() payable {
+        challenge = new Secret();
+    }
+
+    function isSolved() external view returns (bool) {
+        return challenge.isChallengeSolved();
+    }
+}
